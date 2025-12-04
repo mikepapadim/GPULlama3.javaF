b202bb41eb59caf1013905ac98c48655f00ec177
mikepapadim <mikepapadim@hotmail.com>
2025-12-04 16:11:44 +0200

    Refactor FP16 FFN layers to streamline task graph setup, update worker grid assignments, and enhance attention and FFN block configurations.

--- src/main/java/org/beehive/gpullama3/tornadovm/layers/type/fp16/LlamaFP16FFNLayers.java
+++ src/main/java/org/beehive/gpullama3/tornadovm/layers/type/fp16/LlamaFP16FFNLayers.java
@@ -159,10 +159,8 @@ public class LlamaFP16FFNLayers extends AbstractFFNLayers {
                 config.headSize(),
                 layerIndex,
                 config.contextLength());
-
         // Attention
         configureAttention(unifiedLayer, layerIndex);
-
         // Output Projection (Wo) with residual
         unifiedLayer.task("attn_output_proj",
                 TransformerComputeKernelsLayered::matrixVectorGenericWithResidual,
